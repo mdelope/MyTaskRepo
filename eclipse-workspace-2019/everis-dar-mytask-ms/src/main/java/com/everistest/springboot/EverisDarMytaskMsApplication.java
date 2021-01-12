5793cb9514b849f7658a95ffed4e7755c9394cdb
mdelopea <mdelopea@host.docker.internal>
2021-01-12 15:41:13 +0100

    First Commit

---
+++ eclipse-workspace-2019/everis-dar-mytask-ms/src/main/java/com/everistest/springboot/EverisDarMytaskMsApplication.java
@@ -0,0 +1,13 @@
+package com.everistest.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverisDarMytaskMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverisDarMytaskMsApplication.class, args);
+	}
+
+}
